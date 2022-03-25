4076254a0b68ee498f09ccbab9cc6709126f51f3
mdaffa48 <work@muhammaddaffa.com>
2022-03-25 10:03:43 +0700

    Initial commit

---
+++ src/main/java/com/muhammaddaffa/serverdonations/ServerDonation.java
@@ -0,0 +1,12 @@
+package com.muhammaddaffa.serverdonations;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ServerDonation extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+}
