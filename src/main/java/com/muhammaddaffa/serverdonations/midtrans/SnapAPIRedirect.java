edddf11ab182f48105312b2f491f677fd240ace8
mdaffa48 <work@muhammaddaffa.com>
2022-03-26 12:31:48 +0700

    Updating the startReceivingNotifications method

--- src/main/java/com/muhammaddaffa/serverdonations/midtrans/SnapAPIRedirect.java
+++ src/main/java/com/muhammaddaffa/serverdonations/midtrans/SnapAPIRedirect.java
@@ -1,18 +1,24 @@
 package com.muhammaddaffa.serverdonations.midtrans;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.midtrans.Config;
 import com.midtrans.ConfigFactory;
 import com.midtrans.httpclient.error.MidtransError;
 import com.midtrans.service.MidtransSnapApi;
 import com.muhammaddaffa.serverdonations.midtrans.body.RequestBodyBuilder;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.eclipse.jetty.http.HttpStatus;
+import org.json.JSONObject;
 import spark.Spark;
 
+import java.beans.PersistenceDelegate;
 import java.util.*;
 
 public class SnapAPIRedirect {
 
+    private final Gson gson = new Gson();
     private final MidtransSnapApi snapAPI;
 
     public SnapAPIRedirect(String serverKey, String clientKey, boolean production){
@@ -38,10 +44,23 @@ public class SnapAPIRedirect {
     public void startReceivingNotifications(){
         Spark.get("/midtrans/notification", ((request, response) -> {
 
-            System.out.println(request);
-            System.out.println(response);
+            JSONObject json = this.gson.fromJson(request.body(), JSONObject.class);
 
-            return HttpStatus.OK_200;
+            String orderId = json.getString("order_id");
+
+            String transactionStatus = json.getString("transaction_status");
+            String fraudStatus = json.getString("fraud_status");
+            String statusCode = json.getString("status_code");
+
+            if(transactionStatus.equals("capture") &&
+                    fraudStatus.equals("accept") &&
+                    statusCode.equals("200")){
+
+                Bukkit.broadcastMessage(orderId + " telah dibayar!");
+            }
+
+            response.status(200);
+            return "";
         }));
     }
 
@@ -50,7 +69,9 @@ public class SnapAPIRedirect {
     }
 
     private String generateOrderId(Player player){
-        return player.getName() + "_" + UUID.randomUUID();
+        String orderId = player.getName() + "_" + UUID.randomUUID();
+        Bukkit.broadcastMessage("Order ID: " + orderId);
+        return orderId;
     }
 
 }
