5e996177dfeffa6a1767b76811806714be9d0de5
mdaffa48 <work@muhammaddaffa.com>
2022-03-26 20:49:42 +0700

    Add a bunch of new config values

--- src/main/java/com/muhammaddaffa/serverdonations/configs/ConfigValue.java
+++ src/main/java/com/muhammaddaffa/serverdonations/configs/ConfigValue.java
@@ -4,22 +4,28 @@ import org.bukkit.configuration.file.FileConfiguration;
 
 public class ConfigValue {
 
+    public static boolean DEBUG;
+
+    public static boolean IS_PRODUCTION_MODE;
     public static String PRODUCTION_CLIENT_KEY;
     public static String PRODUCTION_SERVER_KEY;
-
     public static String SANDBOX_CLIENT_KEY;
     public static String SANDBOX_SERVER_KEY;
 
-    public static boolean IS_PRODUCTION_MODE;
+    public static int PORT;
+    public static String PATH;
 
     public static void init(FileConfiguration config){
-        PRODUCTION_CLIENT_KEY = config.getString("production-client-key");
-        PRODUCTION_SERVER_KEY = config.getString("production-server-key");
+        DEBUG = config.getBoolean("debug");
 
-        SANDBOX_CLIENT_KEY = config.getString("sandbox-client-key");
-        SANDBOX_SERVER_KEY = config.getString("sandbox-server-key");
+        IS_PRODUCTION_MODE = config.getBoolean("midtrans.production-mode");
+        PRODUCTION_CLIENT_KEY = config.getString("midtrans.production.client-key");
+        PRODUCTION_SERVER_KEY = config.getString("midtrans.production.server-key");
+        SANDBOX_CLIENT_KEY = config.getString("midtrans.sandbox.client-key");
+        SANDBOX_SERVER_KEY = config.getString("midtrans.sandbox.server-key");
 
-        IS_PRODUCTION_MODE = config.getBoolean("production-mode");
+        PORT = config.getInt("port", 80);
+        PATH = config.getString("notification-path", "/payment/notification");
     }
 
 }
