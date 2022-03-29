ce4d2606f0a00b3a0c5806a5877a1df0f0cd1542
mdaffa48 <work@muhammaddaffa.com>
2022-03-29 20:19:27 +0700

    Add setKey method

--- src/main/java/com/muhammaddaffa/serverdonations/products/Product.java
+++ src/main/java/com/muhammaddaffa/serverdonations/products/Product.java
@@ -4,17 +4,23 @@ import java.util.List;
 
 public class Product {
 
+    private final String key;
     private final String name;
     private final String displayName;
     private final int price;
 
     private final List<String> commands;
 
-    public Product(String name, String displayName, int price, List<String> commands) {
+    public Product(String key, String name, String displayName, int price, List<String> commands) {
+        this.key = key;
         this.name = name;
         this.displayName = displayName;
         this.price = price;
         this.commands = commands;
+    }
+
+    public String getKey(){
+        return key;
     }
 
     public String getName() {
