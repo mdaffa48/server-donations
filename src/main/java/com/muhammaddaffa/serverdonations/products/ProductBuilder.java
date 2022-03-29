ec2c076869a77823974555cc0529f2d4977a3755
mdaffa48 <work@muhammaddaffa.com>
2022-03-29 20:19:52 +0700

    Add setKey method

--- src/main/java/com/muhammaddaffa/serverdonations/products/ProductBuilder.java
+++ src/main/java/com/muhammaddaffa/serverdonations/products/ProductBuilder.java
@@ -6,10 +6,16 @@ import java.util.Objects;
 
 public class ProductBuilder {
 
+    private String key;
     private String name;
     private String displayName;
     private Integer price;
     private List<String> commands = new ArrayList<>();
+
+    public ProductBuilder setKey(String key){
+        this.key = key;
+        return this;
+    }
 
     public ProductBuilder setName(String name){
         this.name = name;
@@ -32,11 +38,13 @@ public class ProductBuilder {
     }
 
     public Product build(){
+        Objects.requireNonNull(key, "Key of the product cannot be null");
         Objects.requireNonNull(name, "Name of the product cannot be null");
         Objects.requireNonNull(displayName, "Display Name of the product cannot be null");
         Objects.requireNonNull(price, "Price of the product cannot be null");
 
         return new Product(
+                key,
                 name,
                 displayName,
                 price,
