9d4fadd90b645200ce4d25c9983a2d8c36215f6a
mdaffa48 <work@muhammaddaffa.com>
2022-05-21 06:46:09 +0700

    Simplify the client and server key

--- src/main/java/com/muhammaddaffa/serverdonations/configs/ConfigValue.java
+++ src/main/java/com/muhammaddaffa/serverdonations/configs/ConfigValue.java
@@ -7,13 +7,22 @@ public class ConfigValue {
     public static boolean DEBUG;
 
     public static boolean IS_PRODUCTION_MODE;
-    public static String PRODUCTION_CLIENT_KEY;
-    public static String PRODUCTION_SERVER_KEY;
-    public static String SANDBOX_CLIENT_KEY;
-    public static String SANDBOX_SERVER_KEY;
+    public static String CLIENT_KEY;
+    public static String SERVER_KEY;
 
     public static int PORT;
     public static String PATH;
+
+    public static boolean MYSQL_ENABLED;
+    public static String MYSQL_HOST;
+    public static int MYSQL_PORT;
+    public static String MYSQL_DATABASE;
+    public static String MYSQL_USER;
+    public static String MYSQL_PASSWORD;
+    public static boolean MYSQL_SSL;
+    public static int MYSQL_WAIT_TIMEOUT;
+    public static int MYSQL_MAX_LIFETIME;
+    public static boolean MYSQL_KEY_RETRIEVAL;
 
     public static boolean USE_UUID;
 
@@ -21,13 +30,27 @@ public class ConfigValue {
         DEBUG = config.getBoolean("debug");
 
         IS_PRODUCTION_MODE = config.getBoolean("midtrans.production-mode");
-        PRODUCTION_CLIENT_KEY = config.getString("midtrans.production.client-key");
-        PRODUCTION_SERVER_KEY = config.getString("midtrans.production.server-key");
-        SANDBOX_CLIENT_KEY = config.getString("midtrans.sandbox.client-key");
-        SANDBOX_SERVER_KEY = config.getString("midtrans.sandbox.server-key");
+        if (IS_PRODUCTION_MODE) {
+            CLIENT_KEY = config.getString("midtrans.production.client-key");
+            SERVER_KEY = config.getString("midtrans.production.server-key");
+        } else {
+            CLIENT_KEY = config.getString("midtrans.sandbox.client-key");
+            SERVER_KEY = config.getString("midtrans.sandbox.server-key");
+        }
 
         PORT = config.getInt("port", 80);
         PATH = config.getString("notification-path", "/payment/notification");
+
+        MYSQL_ENABLED = config.getBoolean("mysql.enabled");
+        MYSQL_HOST = config.getString("mysql.host");
+        MYSQL_PORT = config.getInt("mysql.port");
+        MYSQL_DATABASE = config.getString("mysql.database");
+        MYSQL_USER = config.getString("mysql.user");
+        MYSQL_PASSWORD = config.getString("mysql.password");
+        MYSQL_SSL = config.getBoolean("mysql.useSSL");
+        MYSQL_WAIT_TIMEOUT = config.getInt("mysql.hikari.waitTimeout");
+        MYSQL_MAX_LIFETIME = config.getInt("mysql.hikari.maxLifetime");
+        MYSQL_KEY_RETRIEVAL = config.getBoolean("mysql.hikari.publicKeyRetrieval");
 
         USE_UUID = config.getBoolean("use-uuid");
     }
