2055c975de4fe0094a4e789c5026d1c4ac0133d4
mdaffa48 <work@muhammaddaffa.com>
2022-05-21 07:43:42 +0700

    Add product broadcast method

--- src/main/java/com/muhammaddaffa/serverdonations/products/Product.java
+++ src/main/java/com/muhammaddaffa/serverdonations/products/Product.java
@@ -1,41 +1,67 @@
 package com.muhammaddaffa.serverdonations.products;
 
+import com.muhammaddaffa.serverdonations.configs.ConfigValue;
+import com.muhammaddaffa.serverdonations.utils.ImageChar;
+import com.muhammaddaffa.serverdonations.utils.ImageMessage;
+import com.muhammaddaffa.serverdonations.utils.Utils;
+import me.aglerr.mclibs.libs.Common;
+import me.aglerr.mclibs.libs.Executor;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
-public class Product {
+public record Product(
+        String key,
+        String displayName,
+        int price,
+        List<String> commands
+) {
 
-    private final String key;
-    private final String name;
-    private final String displayName;
-    private final int price;
+    public void broadcast(Player player) {
+        Executor.async(() -> {
+            try {
 
-    private final List<String> commands;
+                if (ConfigValue.USE_AVATAR_MESSAGE) {
+                    URL url = new URL(Utils.getMinepicURL(player));
+                    BufferedImage image = ImageIO.read(url);
 
-    public Product(String key, String name, String displayName, int price, List<String> commands) {
-        this.key = key;
-        this.name = name;
-        this.displayName = displayName;
-        this.price = price;
-        this.commands = commands;
+                    String[] additional = new String[]{
+                            this.parse(ConfigValue.AVATAR_LINE1),
+                            this.parse(ConfigValue.AVATAR_LINE2),
+                            this.parse(ConfigValue.AVATAR_LINE3),
+                            this.parse(ConfigValue.AVATAR_LINE4),
+                            this.parse(ConfigValue.AVATAR_LINE5),
+                            this.parse(ConfigValue.AVATAR_LINE6),
+                            this.parse(ConfigValue.AVATAR_LINE7),
+                            this.parse(ConfigValue.AVATAR_LINE8)
+                    };
+
+                    // Finally, send the message
+                    new ImageMessage(image, 8, ImageChar.BLOCK.getChar())
+                            .appendText(additional)
+                            .sendMessage();
+                } else {
+
+                    // Normal messages
+                    ConfigValue.NORMAL_MESSAGES.forEach(message ->
+                            Bukkit.broadcastMessage(Common.color(message)));
+                }
+
+            } catch (IOException ex) {
+                Common.log("&cFailed to send broadcast donation");
+                ex.printStackTrace();
+            }
+        });
     }
 
-    public String getKey(){
-        return key;
+    private String parse(String message) {
+        return message.replace("{product_name}", this.displayName())
+                .replace("{product_price}", Utils.formatNumber(this.price()));
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public List<String> getCommands() {
-        return commands;
-    }
 }
