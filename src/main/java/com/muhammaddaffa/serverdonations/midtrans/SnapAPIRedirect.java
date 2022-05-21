9248d6253d46f83d4cf9057c184bfcae7fa16f87
mdaffa48 <work@muhammaddaffa.com>
2022-05-21 10:29:36 +0700

    Implement actual donation system

--- src/main/java/com/muhammaddaffa/serverdonations/midtrans/SnapAPIRedirect.java
+++ src/main/java/com/muhammaddaffa/serverdonations/midtrans/SnapAPIRedirect.java
@@ -5,41 +5,76 @@ import com.midtrans.ConfigFactory;
 import com.midtrans.httpclient.error.MidtransError;
 import com.midtrans.service.MidtransSnapApi;
 import com.muhammaddaffa.serverdonations.configs.ConfigValue;
+import com.muhammaddaffa.serverdonations.database.SQLDatabaseInitializer;
 import com.muhammaddaffa.serverdonations.midtrans.helpers.JsonObjectWrapper;
 import com.muhammaddaffa.serverdonations.midtrans.helpers.RequestBodyBuilder;
 import com.muhammaddaffa.serverdonations.products.Product;
+import com.muhammaddaffa.serverdonations.products.ProductManager;
 import com.muhammaddaffa.serverdonations.transactions.OrderId;
-import com.muhammaddaffa.serverdonations.utils.Utils;
+import me.aglerr.mclibs.libs.Common;
 import me.aglerr.mclibs.libs.Debug;
+import me.aglerr.mclibs.libs.Executor;
+import me.aglerr.mclibs.mysql.SQLHelper;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.jetbrains.annotations.NotNull;
 import spark.Spark;
 
+import java.sql.ResultSet;
 import java.util.*;
 
-public class SnapAPIRedirect {
+public class SnapAPIRedirect implements Listener {
 
     private final MidtransSnapApi snapAPI;
+    private final ProductManager productManager;
 
-    public SnapAPIRedirect(String serverKey, String clientKey, boolean production) {
+    private final Map<String, Product> pendingDonations = new HashMap<>();
+
+    public SnapAPIRedirect(String serverKey, String clientKey, boolean production, ProductManager productManager) {
         this.snapAPI = new ConfigFactory(new Config(serverKey, clientKey, production))
                 .getSnapApi();
+        this.productManager = productManager;
     }
 
-    public String generateInvoiceURL(Player player, Product product) throws MidtransError {
-        return this.snapAPI
-                .createTransactionRedirectUrl(this.requestBody(player, product));
+    public String generateInvoiceURL(@NotNull Player player, @NotNull Product product) {
+        try {
+            String orderId = OrderId.generate(player);
+            String query = "INSERT INTO `" + SQLDatabaseInitializer.TRANSACTION_TABLE + "` " +
+                    "(customer_name, order_id, product, status) " +
+                    "VALUES (?, ?, ?, ?);";
+
+            SQLHelper.buildStatement(query, statement -> {
+                statement.setString(1, player.getName());
+                statement.setString(2, orderId);
+                statement.setString(3, product.key());
+                statement.setString(4, "UNPAID");
+
+                statement.execute();
+
+            }, ex -> {
+                Common.log("&cFailed to generate Redirect URL");
+                ex.printStackTrace();
+            });
+
+            Map<String, Object> body = new RequestBodyBuilder()
+                    .setTransactionDetails(orderId, product)
+                    .setCustomerDetails(player.getName())
+                    .addItemDetail(product)
+                    .build();
+
+            return this.snapAPI.createTransactionRedirectUrl(body);
+
+        } catch (MidtransError ex) {
+            Common.log("&cCouldn't generate invoice URL");
+            ex.printStackTrace();
+        }
+        return null;
     }
 
-    private Map<String, Object> requestBody(Player player, Product product) {
-        return new RequestBodyBuilder()
-                .setTransactionDetails(OrderId.generate(player), product)
-                .setCustomerDetails(player.getName())
-                .addItemDetail(product)
-                .build();
-    }
-
-    public void startReceivingNotifications() {
+    public void handleNotification() {
         Spark.port(ConfigValue.PORT);
         Spark.init();
 
@@ -63,25 +98,117 @@ public class SnapAPIRedirect {
                     " "
             );
 
-            if(this.isStatusGood(transactionStatus) &&
+            if (this.isStatusGood(transactionStatus) &&
                     fraudStatus.equals("accept") &&
-                    statusCode.equals("200")){
+                    statusCode.equals("200")) {
 
-                Bukkit.broadcastMessage(orderId + " telah dibayar!");
-                Bukkit.getConsoleSender().sendMessage("broadcast &a&laglerr just donated to the server!");
+                Executor.async(() -> {
+                    String query = "SELECT * FROM `" + SQLDatabaseInitializer.TRANSACTION_TABLE + "` " +
+                            "WHERE order_id=?";
+
+                    SQLHelper.buildStatement(query, statement -> {
+                        statement.setString(1, orderId);
+
+                        ResultSet result = statement.executeQuery();
+                        if (result.next()) {
+                            String playerName = result.getString("customer_name");
+                            String productKey = result.getString("product");
+
+                            Player player = Bukkit.getPlayer(playerName);
+                            Product product = this.productManager.getProduct(productKey);
+
+                            this.updateTransaction(orderId);
+
+                            if (player == null) {
+                                this.pendingDonations.put(playerName, product);
+                                return;
+                            }
+
+                            if (product != null) {
+                                product.broadcast(player);
+                                this.updateUser(player, product);
+                            }
+                        }
+                    }, ex -> {
+                        Common.log("&cFailed to find transaction with order id (" + orderId + ")");
+                        ex.printStackTrace();
+                    });
+                });
+
             }
 
-            response.status(200);
             return "";
         }));
     }
 
-    public void stopReceivingNotifications() {
-        Spark.stop();
+    @EventHandler()
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        Product product = this.pendingDonations.remove(player.getName());
+
+        Executor.async(() -> {
+            if (product != null) {
+                product.broadcast(player);
+
+                this.updateUser(player, product);
+            }
+        });
     }
 
-    private boolean isStatusGood(String status){
-        return status.equals("cature") || status.equals("settlement");
+    private void updateTransaction(String orderId) {
+        String query = "UPDATE `" + SQLDatabaseInitializer.TRANSACTION_TABLE + "` " +
+                "SET `status`=? " +
+                "WHERE `order_id`=?;";
+
+        SQLHelper.buildStatement(query, statement -> {
+            statement.setString(1, "PAID");
+            statement.setString(2, orderId);
+
+            statement.execute();
+
+        }, ex -> {
+            Common.log("&cFailed to set transaction status");
+            ex.printStackTrace();
+        });
+    }
+
+    private void updateUser(Player player, Product product) {
+        String query = "SELECT * FROM `" + SQLDatabaseInitializer.USER_TABLE + "` " +
+                "WHERE `name`=?";
+
+        SQLHelper.buildStatement(query, statement -> {
+            statement.setString(1, player.getName());
+
+            ResultSet result = statement.executeQuery();
+            int total = product.price();
+
+            if (result.next()) {
+                total += result.getInt("total");
+            }
+
+            String updateQuery = "REPLACE INTO `" + SQLDatabaseInitializer.USER_TABLE + "` " +
+                    "SET `total`=? " +
+                    "WHERE `name`=?;";
+
+            final int finalTotal = total;
+
+            SQLHelper.buildStatement(updateQuery, updateStatement -> {
+                updateStatement.setInt(1, finalTotal);
+                updateStatement.setString(2, player.getName());
+
+                updateStatement.execute();
+
+            }, ex -> {
+                Common.log("&cFailed to update '" + player.getName() + "' total donation");
+                ex.printStackTrace();
+            });
+
+        }, ex -> {
+        });
+    }
+
+    private boolean isStatusGood(String status) {
+        return status.equals("capture") || status.equals("settlement");
     }
 
 }
