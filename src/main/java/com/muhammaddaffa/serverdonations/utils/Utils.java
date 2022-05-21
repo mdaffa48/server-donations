72efe48a36f2cd22e4a4f1ecd1cc8dbfd4547e12
mdaffa48 <work@muhammaddaffa.com>
2022-05-21 07:39:18 +0700

    Add Utils#formatNumber method

--- src/main/java/com/muhammaddaffa/serverdonations/utils/Utils.java
+++ src/main/java/com/muhammaddaffa/serverdonations/utils/Utils.java
@@ -5,10 +5,13 @@ import net.skinsrestorer.api.SkinsRestorerAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.text.DecimalFormat;
 import java.util.UUID;
 import java.util.logging.Level;
 
 public class Utils {
+
+    private static DecimalFormat format = new DecimalFormat("###,###,###,###,###.##");
 
     public static String getMinepicURL(Player player) {
         String url = "https://minepic.org/avatar/8/";
@@ -20,6 +23,10 @@ public class Utils {
             url = url + (player.getName());
         }
         return url;
+    }
+
+    public static String formatNumber(int number) {
+        return format.format(number);
     }
 
     public static String generateOrderId(Player player){
