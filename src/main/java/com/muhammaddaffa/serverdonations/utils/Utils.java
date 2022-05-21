20d0eee8ec13c62fa82c63b3e071df84bfd9c64b
mdaffa48 <work@muhammaddaffa.com>
2022-05-21 07:34:14 +0700

    Add Utils#getMinepicURL method

--- src/main/java/com/muhammaddaffa/serverdonations/utils/Utils.java
+++ src/main/java/com/muhammaddaffa/serverdonations/utils/Utils.java
@@ -1,5 +1,7 @@
 package com.muhammaddaffa.serverdonations.utils;
 
+import com.muhammaddaffa.serverdonations.hooks.SkinsRestorerHook;
+import net.skinsrestorer.api.SkinsRestorerAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
@@ -7,6 +9,18 @@ import java.util.UUID;
 import java.util.logging.Level;
 
 public class Utils {
+
+    public static String getMinepicURL(Player player) {
+        String url = "https://minepic.org/avatar/8/";
+        // Check if SkinsRestorer is enabled
+        if (SkinsRestorerHook.isEnabled()) {
+            SkinsRestorerAPI api = SkinsRestorerAPI.getApi();
+            url = url + (api.hasSkin(player.getName()) ? api.getSkinName(player.getName()) : player.getName());
+        } else {
+            url = url + (player.getName());
+        }
+        return url;
+    }
 
     public static String generateOrderId(Player player){
         return generateOrderId(player.getName());
