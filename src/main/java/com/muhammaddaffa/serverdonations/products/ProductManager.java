a2070b0effa41786215183b70ac547575097c2b6
mdaffa48 <work@muhammaddaffa.com>
2022-05-21 07:11:47 +0700

    Creating ProductManager.java

--- src/main/java/com/muhammaddaffa/serverdonations/products/ProductManager.java
+++ src/main/java/com/muhammaddaffa/serverdonations/products/ProductManager.java
@@ -1,4 +1,43 @@
 package com.muhammaddaffa.serverdonations.products;
 
+import com.muhammaddaffa.serverdonations.configs.ConfigManager;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ProductManager {
+
+    private final Map<String, Product> productMap = new HashMap<>();
+
+    @Nullable
+    public Product getProduct(String key) {
+        return this.productMap.get(key);
+    }
+
+    public void load() {
+        // Clear the product first
+        this.productMap.clear();
+
+        FileConfiguration config = ConfigManager.PRODUCTS.getConfig();
+        if (!config.isConfigurationSection("products")) {
+            return;
+        }
+        for (String key : config.getConfigurationSection("products").getKeys(false)) {
+            String displayName = config.getString("products." + key + ".display-name");
+            int price = config.getInt("products." + key + ".price");
+            List<String> commands = config.getStringList("products." + key + ".commands");
+
+            this.productMap.put(key, new ProductBuilder()
+                            .setKey(key)
+                            .setDisplayName(displayName)
+                            .setPrice(price)
+                            .setCommands(commands)
+                            .build()
+            );
+        }
+    }
+
 }
